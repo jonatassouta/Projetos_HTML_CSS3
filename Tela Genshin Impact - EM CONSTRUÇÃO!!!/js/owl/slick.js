cc50a6751ede83811093394cccf8ccb658623aac
jonatassouta <jonatas.souta81@gmail.com>
2021-09-13 19:24:45 -0300

    Tela Genshin Impact 2.0 AINDA EM CONSTRUÇÃOgit add .git add .

---
+++ Tela Genshin Impact - EM CONSTRUÇÃO!!!/js/owl/slick.js
@@ -0,0 +1,21 @@
+$('.carocel').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+});
+
+$('.carocel1').slick({
+    dots: false,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+});
+
+$('.fade').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
